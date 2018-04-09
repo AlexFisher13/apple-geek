e64eda1df5ddfbcbd3269bc085716a83e27c6508
Tkachenko AI <alexfisher13@yandex.ru>
2018-04-09 13:17:44 +0300

    reafactoring image

--- src/containers/iPhone.js
+++ src/containers/iPhone.js
@@ -7,19 +7,6 @@ class Iphone extends Component {
   render() {
     return (
        <div className='container'>
-           {/*<div className='sidebar'>*/}
-               {/*<button>2007</button>*/}
-               {/*<button>2008</button>*/}
-               {/*<button>2009</button>*/}
-               {/*<button>2010</button>*/}
-               {/*<button>2011</button>*/}
-               {/*<button>2012</button>*/}
-               {/*<button>2013</button>*/}
-               {/*<button>2014</button>*/}
-               {/*<button>2015</button>*/}
-               {/*<button>2016</button>*/}
-               {/*<button>2017</button>*/}
-           {/*</div>*/}
            <div className="grid">
               {
                  this.props.iphones.map((iphone,i) =>
