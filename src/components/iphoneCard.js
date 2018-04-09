a0835c2f8a6b11b9db65c146e51daa98f74892e2
Tkachenko AI <alexfisher13@yandex.ru>
2018-04-09 14:40:10 +0300

    reafactoring iphones

--- src/components/iphoneCard.js
+++ src/components/iphoneCard.js
@@ -9,7 +9,7 @@ export default class IphoneCard extends Component {
                 <p><span className='name'>{i.name}</span></p>
                 <img src={i.img} alt=""/>
                 <hr/>
-                <p><small>Инновация: </small>{i.innovation.split('; ').map((innov, i) =>
+                <p>{i.feature.split('; ').map((innov, i) =>
                     <mark key={i}>{innov}</mark>)}</p>
                 <p><small>Дисплей: </small>{i.screen}</p>
                 <p><small>Камера: </small>{i.cam}</p>
